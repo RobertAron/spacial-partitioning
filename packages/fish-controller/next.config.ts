bf6e2d9ceb9ea3f9dccf09342e1384351c34940d
Robert Aron <contact@robertaron.io>
2025-07-02 14:16:48 -0500

    turboize

--- packages/fish-controller/next.config.ts
+++ packages/fish-controller/next.config.ts
@@ -1,25 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	webpack: (config, { webpack }) => {
-		// config.experiments = {
-		// 	...config.experiments,
-		// 	topLevelAwait: true,
-		// 	asyncWebAssembly: true,
-		// };
-		config.externals["node:fs"] = "commonjs node:fs";
-		config.resolve.fallback = {
-			...config.resolve.fallback,
-			fs: false,
-		};
-		config.plugins.push(
-			new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
-				resource.request = resource.request.replace(/^node:/, "");
-			}),
-		);
+  webpack: (config, { webpack }) => {
+    // config.experiments = {
+    // 	...config.experiments,
+    // 	topLevelAwait: true,
+    // 	asyncWebAssembly: true,
+    // };
+    config.externals["node:fs"] = "commonjs node:fs";
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    config.plugins.push(
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      new webpack.NormalModuleReplacementPlugin(/^node:/, (resource: any) => {
+        resource.request = resource.request.replace(/^node:/, "");
+      })
+    );
 
-		return config;
-	}
+    return config;
+  },
 };
 
 export default nextConfig;
